33688111b19fd255cbdd3ab0477e56434d0f5bf8
rynjwssl <ryan.wessel@student.fairfield.edu>
2015-08-29 13:51:06 -0400

    attempting in jquery

--- js/custom.js
+++ js/custom.js
@@ -1,45 +1,43 @@
 // BEGIN CUSTOM JAVASCRIPT/jQuery HERE
 
-function notePush() {
-  event.preventDefault();
+// function notePush() {
+//   event.preventDefault();
+//
+//   // Set empty notes array
+//   var notes = [];
+//   // Get value from text input
+//   var note = document.getElementById('note-content').value;
+//   // Push most recent note into notes array
+//   notes.push(note);
+//   // Submit note to LocalStorage
+//   localStorage.setItem('notes', JSON.stringify(notes));
+//
+// };
 
-  // Set empty notes array
-  var notes = [];
-  // Get value from text input
-  var note = document.getElementById('note-content').value;
-  // Push most recent note into notes array
-  notes.push(note);
-  // Submit note to LocalStorage
-  localStorage.setItem('notes', JSON.stringify(notes));
+$(function notePush(a) {
+  a.preventDefault();
 
-};
+  if ('localStorage' in window && window['localStorage'] !== null) {
+      try {
+          //Get value from text input
+          var note = $('#note-content')[0].val();
 
-// $(function() {
-//   $('note-submit').on('submit', function(a) {
-//     a.preventDefault();
-//
-//     if ('localStorage' in window && window['localStorage'] !== null) {
-//         try {
-//             //Get value from text input
-//             var note = $('#note-content')[0].val();
-//
-//             //Parse the notes, create an array so I can create multiple notes
-//             notes = localStorage.getItem('note');
-//
-//             notes = notes ? JSON.parse(notes) : [];
-//
-//             notes.push(note);
-//
-//             localStorage.setItem("note", JSON.stringify(notes));
-//             console.log("The data was saved.");
-//             return true;
-//         } catch (error) {
-//             if (error == QUOTA_EXCEEDED_ERR) {
-//                 alert('Quota exceeded!');
-//             }
-//         }
-//         } else {
-//             alert('Cannot store user preferences as your browser do not support local storage');
-//         }
-//   })
-// });
+          //Parse the notes, create an array so I can create multiple notes
+          notes = localStorage.getItem('note');
+
+          notes = notes ? JSON.parse(notes) : [];
+
+          notes.push(note);
+
+          localStorage.setItem("note", JSON.stringify(notes));
+          console.log("The data was saved.");
+          return true;
+      } catch (error) {
+          if (error == QUOTA_EXCEEDED_ERR) {
+              alert('Quota exceeded!');
+          }
+      }
+      } else {
+          alert('Cannot store user preferences as your browser do not support local storage');
+      }
+});
