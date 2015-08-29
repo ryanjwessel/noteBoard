eb92dfe86db8fde9ef8578b8803baa8589ab70fb
rynjwssl <ryan.wessel@student.fairfield.edu>
2015-08-29 13:37:08 -0400

    simplified note submission

--- js/custom.js
+++ js/custom.js
@@ -1,31 +1,41 @@
 // BEGIN CUSTOM JAVASCRIPT/jQuery HERE
 
-$(function() {
-  $('note-submit').on('submit', function(a) {
-    a.preventDefault();
+function notePush() {
+  event.preventDefault();
 
-    if ('localStorage' in window && window['localStorage'] !== null) {
-        try {
-            //Get value from text input
-            var note = $('#note-content')[0].val();
+  // Get value from text input
+  var note = document.getElementById('note-content').value;
 
-            //Parse the notes, create an array so I can create multiple notes
-            notes = localStorage.getItem('note');
+  var notes = localStorage.setItem("note");
 
-            notes = notes ? JSON.parse(notes) : [];
+};
 
-            notes.push(note);
-
-            localStorage.setItem("note", JSON.stringify(notes));
-            console.log("The data was saved.");
-            return true;
-        } catch (error) {
-            if (error == QUOTA_EXCEEDED_ERR) {
-                alert('Quota exceeded!');
-            }
-        }
-        } else {
-            alert('Cannot store user preferences as your browser do not support local storage');
-        }
-  })
-});
+// $(function() {
+//   $('note-submit').on('submit', function(a) {
+//     a.preventDefault();
+//
+//     if ('localStorage' in window && window['localStorage'] !== null) {
+//         try {
+//             //Get value from text input
+//             var note = $('#note-content')[0].val();
+//
+//             //Parse the notes, create an array so I can create multiple notes
+//             notes = localStorage.getItem('note');
+//
+//             notes = notes ? JSON.parse(notes) : [];
+//
+//             notes.push(note);
+//
+//             localStorage.setItem("note", JSON.stringify(notes));
+//             console.log("The data was saved.");
+//             return true;
+//         } catch (error) {
+//             if (error == QUOTA_EXCEEDED_ERR) {
+//                 alert('Quota exceeded!');
+//             }
+//         }
+//         } else {
+//             alert('Cannot store user preferences as your browser do not support local storage');
+//         }
+//   })
+// });
