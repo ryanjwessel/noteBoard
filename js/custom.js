0bbec32b49584773ce86b1de08131495f7c1c35e
rynjwssl <ryan.wessel@student.fairfield.edu>
2015-08-30 19:45:16 -0400

    append note?

--- js/custom.js
+++ js/custom.js
@@ -19,13 +19,16 @@ function notePush() {
     var parseNotes = JSON.parse(localStorage.getItem('note'));
     console.log(parseNotes);
 
+    $(".note-container").append('<div class="row note-wrap"><div class="row note"><div class="col-xs-12 col-sm-12"><p class="notes note-text">'+note+'</p></div></div></div>');
+
+
 };
 
 
-$( document ).ready(function(parseNotes) {
-
-  for(i = 0; i < parseNotes.length; i++) {
-    $(".note-container").append('<div class="row note-wrap"><div class="row note"><div class="col-xs-12 col-sm-12"><p class="notes note-text">'+parseNotes[i]+'</p></div></div></div>');
-  };
-  
-});
+// $( document ).ready(function(parseNotes) {
+//
+//   for(i = 0; i < parseNotes.length; i++) {
+//     $(".note-container").append('<div class="row note-wrap"><div class="row note"><div class="col-xs-12 col-sm-12"><p class="notes note-text">'+parseNotes[i]+'</p></div></div></div>');
+//   };
+//
+// });
