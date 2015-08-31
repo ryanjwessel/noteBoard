939ae19a81537e5d12677c4cffc99effe5dba5ef
rynjwssl <ryan.wessel@student.fairfield.edu>
2015-08-30 22:07:53 -0400

    changed unique classes to ids

--- js/custom.js
+++ js/custom.js
@@ -8,7 +8,7 @@ $( document ).ready(function() {
 
   //Loop through notes and display in rows
   for(i = 0; i < noteStart.length; i++) {
-    $(".note-container").append('<div class="row note-wrap note-wrap-'+i+'"><div class="row note"><div class="col-xs-offset-1 col-sm-offset-1 col-xs-9 col-sm-10"><p class="notes notes-'+i+' note-text '+i+'">'+noteStart[i]+'</p></div><div class="col-xs-2 col-sm-1 text-right"><input type="button" class="note-delete note-delete-'+i+' btn" onclick="noteDelete();" value="X"></input></div></div></div>');
+    $(".note-container").append('<div class="row note-wrap" id="note-wrap-'+i+'"><div class="row note"><div class="col-xs-offset-1 col-sm-offset-1 col-xs-9 col-sm-10"><p class="notes note-text" id="notes-'+i+'">'+noteStart[i]+'</p></div><div class="col-xs-2 col-sm-1 text-right"><input type="button" class="note-delete btn" id="note-delete-'+i+'" onclick="noteDelete();" value="X"></input></div></div></div>');
   };
 
 });
@@ -36,7 +36,7 @@ function notePush() {
     console.log(i);
 
     //Append the notes in rows
-    $(".note-container").append('<div class="row note-wrap note-wrap-'+i+'"><div class="row note"><div class="col-xs-offset-1 col-sm-offset-1 col-xs-9 col-sm-10"><p class="notes notes-'+i+' note-text">'+note+'</p></div><div class="col-xs-2 col-sm-1 text-right"><input type="button" class="note-delete delete note-delete-'+i+' btn" onclick="noteDelete();" value="X"></input></div></div></div>');
+    $(".note-container").append('<div class="row note-wrap" id="note-wrap-'+i+'"><div class="row note"><div class="col-xs-offset-1 col-sm-offset-1 col-xs-9 col-sm-10"><p class="notes note-text" id="notes-'+i+'">'+note+'</p></div><div class="col-xs-2 col-sm-1 text-right"><input type="button" class="note-delete btn" id="note-delete-'+i+'" onclick="noteDelete();" value="X"></input></div></div></div>');
     $('.note-wrap-' + i).css('display', 'none');
     $('.note-wrap-' + i).slideDown();
 
