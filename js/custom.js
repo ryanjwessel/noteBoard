8eb7ca2ccc60b18c3c4ee55234f80df0f84b0a9f
rynjwssl <ryan.wessel@student.fairfield.edu>
2015-08-30 21:57:28 -0400

    fixed slide css

--- js/custom.js
+++ js/custom.js
@@ -36,8 +36,8 @@ function notePush() {
     console.log(i);
 
     //Append the notes in rows
-    $(".note-container").append('<div class="row note-wrap"><div class="row note"><div class="col-xs-offset-1 col-sm-offset-1 col-xs-9 col-sm-10"><p class="notes notes-'+i+' note-text">'+note+'</p></div><div class="col-xs-2 col-sm-1 text-right"><input type="button" class="note-delete delete note-delete-'+i+' btn" onclick="noteDelete();" value="X"></input></div></div></div>');
-    $('.notes-' + i).slideDown();
+    $(".note-container").append('<div class="row note-wrap note-wrap-'+i+'"><div class="row note"><div class="col-xs-offset-1 col-sm-offset-1 col-xs-9 col-sm-10"><p class="notes notes-'+i+' note-text">'+note+'</p></div><div class="col-xs-2 col-sm-1 text-right"><input type="button" class="note-delete delete note-delete-'+i+' btn" onclick="noteDelete();" value="X"></input></div></div></div>');
+    $('.note-wrap-' + i).slideDown();
 
     //Reset input value
     $('#note-content').val("");
