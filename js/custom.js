d75b18ab04f38953facb186a021827a22d5ad38f
rynjwssl <ryan.wessel@student.fairfield.edu>
2015-08-30 21:15:22 -0400

    added array index to delete button

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,9 @@
 // BEGIN CUSTOM JAVASCRIPT/jQuery HERE
 
 function notePush() {
+
+    var i = 0;
+
     //Get value from text input
     var note = JSON.stringify($("#note-content").val()),
 
@@ -20,10 +23,12 @@ function notePush() {
     console.log(parseNotes);
 
     //Append the notes in rows
-    $(".note-container").append('<div class="row note-wrap"><div class="row note"><div class="col-xs-offset-1 col-sm-offset-1 col-xs-9 col-sm-10"><p class="notes note-text">'+note+'</p></div><div class="col-xs-2 col-sm-1 text-right"><input type="button" class="note-delete btn" onclick="noteDelete();" value="X"></input></div></div></div>');
+    $(".note-container").append('<div class="row note-wrap"><div class="row note"><div class="col-xs-offset-1 col-sm-offset-1 col-xs-9 col-sm-10"><p class="notes note-text">'+note+'</p></div><div class="col-xs-2 col-sm-1 text-right"><input type="button" class="note-delete-'+i+' btn" onclick="noteDelete();" value="X"></input></div></div></div>');
 
     //Reset input value
     $('#note-content').val("");
+
+    i++;
 };
 
 
@@ -34,7 +39,7 @@ $( document ).ready(function() {
 
   //Loop through notes and display in rows
   for(i = 0; i < noteStart.length; i++) {
-    $(".note-container").append('<div class="row note-wrap"><div class="row note"><div class="col-xs-offset-1 col-sm-offset-1 col-xs-9 col-sm-10"><p class="notes note-text '+i+'">'+noteStart[i]+'</p></div><div class="col-xs-2 col-sm-1 text-right"><input type="button" class="note-delete btn" onclick="noteDelete();" value="X"></input></div></div></div>');
+    $(".note-container").append('<div class="row note-wrap"><div class="row note"><div class="col-xs-offset-1 col-sm-offset-1 col-xs-9 col-sm-10"><p class="notes note-text '+i+'">'+noteStart[i]+'</p></div><div class="col-xs-2 col-sm-1 text-right"><input type="button" class="note-delete-'+i+' btn" onclick="noteDelete();" value="X"></input></div></div></div>');
   };
 
 });
