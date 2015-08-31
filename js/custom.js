c67ab7d19f064d64b594d17d742dd55093088a68
rynjwssl <ryan.wessel@student.fairfield.edu>
2015-08-30 21:18:16 -0400

    fixed minor adding error

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,18 @@
 // BEGIN CUSTOM JAVASCRIPT/jQuery HERE
 
-function notePush() {
+$( document ).ready(function() {
 
-    var i = 0;
+  //Load notes in localStorage
+  var noteStart = JSON.parse(localStorage.getItem('note'));
+
+  //Loop through notes and display in rows
+  for(i = 0; i < noteStart.length; i++) {
+    $(".note-container").append('<div class="row note-wrap"><div class="row note"><div class="col-xs-offset-1 col-sm-offset-1 col-xs-9 col-sm-10"><p class="notes note-text '+i+'">'+noteStart[i]+'</p></div><div class="col-xs-2 col-sm-1 text-right"><input type="button" class="note-delete note-delete-'+i+' btn" onclick="noteDelete();" value="X"></input></div></div></div>');
+  };
+
+});
+
+function notePush() {
 
     //Get value from text input
     var note = JSON.stringify($("#note-content").val()),
@@ -21,6 +31,7 @@ function notePush() {
     //Parse the notes to create an array
     var parseNotes = JSON.parse(localStorage.getItem('note'));
     console.log(parseNotes);
+    var i = parseNotes.length;
 
     //Append the notes in rows
     $(".note-container").append('<div class="row note-wrap"><div class="row note"><div class="col-xs-offset-1 col-sm-offset-1 col-xs-9 col-sm-10"><p class="notes note-text">'+note+'</p></div><div class="col-xs-2 col-sm-1 text-right"><input type="button" class="note-delete delete note-delete-'+i+' btn" onclick="noteDelete();" value="X"></input></div></div></div>');
@@ -30,16 +41,3 @@ function notePush() {
 
     i++;
 };
-
-
-$( document ).ready(function() {
-
-  //Load notes in localStorage
-  var noteStart = JSON.parse(localStorage.getItem('note'));
-
-  //Loop through notes and display in rows
-  for(i = 0; i < noteStart.length; i++) {
-    $(".note-container").append('<div class="row note-wrap"><div class="row note"><div class="col-xs-offset-1 col-sm-offset-1 col-xs-9 col-sm-10"><p class="notes note-text '+i+'">'+noteStart[i]+'</p></div><div class="col-xs-2 col-sm-1 text-right"><input type="button" class="note-delete note-delete-'+i+' btn" onclick="noteDelete();" value="X"></input></div></div></div>');
-  };
-
-});
