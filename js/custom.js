9a5843e6ad12b0df93cb1f64f7a139898a9c8c23
rynjwssl <ryan.wessel@student.fairfield.edu>
2015-08-30 22:54:15 -0400

    delete works?

--- js/custom.js
+++ js/custom.js
@@ -7,7 +7,7 @@ $( document ).ready(function() {
 
   //Loop through notes and display in rows
   for(i = 0; i < localStorage.length; i++) {
-    $(".note-container").append('<div class="row note-wrap" id="note-wrap-'+i+'"><div class="row note"><div class="col-xs-offset-1 col-sm-offset-1 col-xs-9 col-sm-10"><p class="notes note-text" id="notes-'+i+'">'+localStorage.getItem("notes-"+i)+'</p><input type="button" class="note-delete btn" id="note-delete-'+i+'" onclick="noteDelete();" value="X"></input></div></div></div>');
+    $(".note-container").append('<div class="row note-wrap" id="note-wrap-'+i+'"><div class="row note"><div class="col-xs-offset-1 col-sm-offset-1 col-xs-9 col-sm-10 note-parent"><p class="notes note-text" id="notes-'+i+'">'+localStorage.getItem("notes-"+i)+'</p><input type="button" class="note-delete btn" id="note-delete-'+i+'" onclick="noteDelete();" value="X"></input></div></div></div>');
   };
 });
 
@@ -21,7 +21,7 @@ function notePush() {
     localStorage.setItem( "notes-" + i, $("#note-content").val());
 
     //Append row with note contents to note-container
-    $(".note-container").append('<div class="row note-wrap" id="note-wrap-'+i+'"><div class="row note"><div class="col-xs-offset-1 col-sm-offset-1 col-xs-9 col-sm-10"><p class="notes note-text" id="notes-'+i+'">'+localStorage.getItem("notes-"+i)+'</p><input type="button" class="note-delete btn" id="note-delete-'+i+'" onclick="noteDelete();" value="X"></input></div></div></div>');
+    $(".note-container").append('<div class="row note-wrap" id="note-wrap-'+i+'"><div class="row note"><div class="col-xs-offset-1 col-sm-offset-1 col-xs-9 col-sm-10 note-parent"><p class="notes note-text" id="notes-'+i+'">'+localStorage.getItem("notes-"+i)+'</p><input type="button" class="note-delete btn" id="note-delete-'+i+'" onclick="noteDelete();" value="X"></input></div></div></div>');
 
     //Add animation effect when submitting new task
     $("#note-wrap-" + i).css('display', 'none');
@@ -38,16 +38,16 @@ function notePush() {
 };
 
 // Remove a note
-$(document).on("click", ".note-delete", function() {
+$(document).on("click", ".note-parent p input.note-delete", function() {
   localStorage.removeItem($(this).parent().attr("id"));
   $(this).parent().slideUp('slow', function() {
     $(this).remove();
   });
   // This part resets all the IDs
   for( i = 0; i < localStorage.length; i++) {
-    if( !localStorage.getItem("task-"+ i)) {
-      localStorage.setItem("task-"+ i, localStorage.getItem('task-' + (i + 1) )); // Moves the id up a level
-      localStorage.removeItem("task-"+ (i + 1) );  // Removes the id 1 up from the deleted item
+    if( !localStorage.getItem("notes-"+ i)) {
+      localStorage.setItem("notes-"+ i, localStorage.getItem('notes-' + (i + 1) )); // Moves the id up a level
+      localStorage.removeItem("notes-"+ (i + 1) );  // Removes the id 1 up from the deleted item
     }
   }
 });
