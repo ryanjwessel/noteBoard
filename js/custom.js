f77510f443331510705817f4a29e3fff7e41099e
rynjwssl <ryan.wessel@student.fairfield.edu>
2015-08-30 21:16:21 -0400

    added original class to restore styling to delete buttons

--- js/custom.js
+++ js/custom.js
@@ -23,7 +23,7 @@ function notePush() {
     console.log(parseNotes);
 
     //Append the notes in rows
-    $(".note-container").append('<div class="row note-wrap"><div class="row note"><div class="col-xs-offset-1 col-sm-offset-1 col-xs-9 col-sm-10"><p class="notes note-text">'+note+'</p></div><div class="col-xs-2 col-sm-1 text-right"><input type="button" class="note-delete-'+i+' btn" onclick="noteDelete();" value="X"></input></div></div></div>');
+    $(".note-container").append('<div class="row note-wrap"><div class="row note"><div class="col-xs-offset-1 col-sm-offset-1 col-xs-9 col-sm-10"><p class="notes note-text">'+note+'</p></div><div class="col-xs-2 col-sm-1 text-right"><input type="button" class="note-delete delete note-delete-'+i+' btn" onclick="noteDelete();" value="X"></input></div></div></div>');
 
     //Reset input value
     $('#note-content').val("");
@@ -39,7 +39,7 @@ $( document ).ready(function() {
 
   //Loop through notes and display in rows
   for(i = 0; i < noteStart.length; i++) {
-    $(".note-container").append('<div class="row note-wrap"><div class="row note"><div class="col-xs-offset-1 col-sm-offset-1 col-xs-9 col-sm-10"><p class="notes note-text '+i+'">'+noteStart[i]+'</p></div><div class="col-xs-2 col-sm-1 text-right"><input type="button" class="note-delete-'+i+' btn" onclick="noteDelete();" value="X"></input></div></div></div>');
+    $(".note-container").append('<div class="row note-wrap"><div class="row note"><div class="col-xs-offset-1 col-sm-offset-1 col-xs-9 col-sm-10"><p class="notes note-text '+i+'">'+noteStart[i]+'</p></div><div class="col-xs-2 col-sm-1 text-right"><input type="button" class="note-delete note-delete-'+i+' btn" onclick="noteDelete();" value="X"></input></div></div></div>');
   };
 
 });
