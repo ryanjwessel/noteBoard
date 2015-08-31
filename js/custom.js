9b3df9b4dd42a0fe4f0912ce143bbaa5da0d642f
rynjwssl <ryan.wessel@student.fairfield.edu>
2015-08-31 01:12:49 -0400

    think it should work now

--- js/custom.js
+++ js/custom.js
@@ -42,6 +42,7 @@ $(document).on("click", ".note-parent p input.note-delete", function() {
   localStorage.removeItem($(this).parent().attr("id"));
   $(this).parent().slideUp('slow', function() {
     $(this).parent().css('display', 'none');
+    $(this).parent().css('margin-bottom', '0px');
     $(this).remove();
   });
   // This part resets all the IDs
