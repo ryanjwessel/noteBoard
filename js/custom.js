164450598fc90d9645b0e13aa413f87f2daea45c
rynjwssl <ryan.wessel@student.fairfield.edu>
2015-08-30 20:17:28 -0400

    removed unnecessary global var, reset input value on submit

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,5 @@
 // BEGIN CUSTOM JAVASCRIPT/jQuery HERE
 
-var noteGlobal = [];
 function notePush() {
     //Get value from text input
     var note = JSON.stringify($("#note-content").val()),
@@ -18,19 +17,22 @@ function notePush() {
 
     //Parse the notes to create an array
     var parseNotes = JSON.parse(localStorage.getItem('note'));
-    noteGlobal = parseNotes;
-    console.log(noteGlobal);
+    console.log(parseNotes);
 
+    //Append the notes in rows
     $(".note-container").append('<div class="row note-wrap"><div class="row note"><div class="col-xs-12 col-sm-12"><p class="notes note-text">'+note+'</p></div></div></div>');
 
-
+    //Reset input value
+    $('note-content').value = "";
 };
 
 
 $( document ).ready(function() {
 
+  //Load notes in localStorage
   var noteStart = JSON.parse(localStorage.getItem('note'));
 
+  //Loop through notes and display in rows
   for(i = 0; i < noteStart.length; i++) {
     $(".note-container").append('<div class="row note-wrap"><div class="row note"><div class="col-xs-12 col-sm-12"><p class="notes note-text '+i+'">'+noteStart[i]+'</p></div></div></div>');
   };
